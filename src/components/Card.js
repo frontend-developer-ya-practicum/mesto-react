722da23002730e2cea10fccd6946565003babac8
Александр Морев <a.morev@kryptonite.ru>
2022-09-19 07:19:33 +0300

    Subscribe Card to CurentUsereContext

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,39 @@
+import { useContext } from "react";
+
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+
 function Card({ card, onCardClick }) {
+  const currentUser = useContext(CurrentUserContext);
+
+  const isOwn = card.owner._id === currentUser._id;
+  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+
+  const cardDeleteButtonClassName = `card__delete-button ${
+    isOwn ? "card__delete-button_visible" : ""
+  }`;
+
+  const cardLikeButtonClassName = `card__like-button ${isLiked ? "card__like-button_active" : ""}`;
+
   function handleClick() {
     onCardClick(card);
   }
 
   return (
     <li className="card">
-      <button className="card__delete-button" type="button" aria-label="Удалить картинку"></button>
+      <button
+        className={cardDeleteButtonClassName}
+        type="button"
+        aria-label="Удалить картинку"
+      ></button>
       <img onClick={handleClick} className="card__image" src={card.link} alt={card.name} />
       <div className="card__content">
         <h2 className="card__title">{card.name}</h2>
         <div className="card__like-container">
-          <button className="card__like-button" type="button" aria-label="Поставить лайк"></button>
+          <button
+            className={cardLikeButtonClassName}
+            type="button"
+            aria-label="Поставить лайк"
+          ></button>
           <span className="card__like-count">{card.likes.length}</span>
         </div>
       </div>
